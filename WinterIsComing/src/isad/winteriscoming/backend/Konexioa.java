41a3f30b924bb7d44ae54af31d2e3c0c9cde0560
unai <uarriza@gmail.com>
2015-10-14 11:40:23 +0200

    erantzuna bueltatzen du artxibo batera

--- WinterIsComing/src/isad/winteriscoming/backend/Konexioa.java
+++ WinterIsComing/src/isad/winteriscoming/backend/Konexioa.java
@@ -1,5 +1,5 @@
 package isad.winteriscoming.backend;
-
+import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -13,11 +13,16 @@ import twitter4j.media.MediaProvider;
 
 import java.awt.Desktop;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Properties;
  
 public class Konexioa {
 	// cb.setDebugEnabled(true)
@@ -28,15 +33,16 @@ public class Konexioa {
  
     public static void main(String[] args) {
  
-        ConfigurationBuilder cb = new ConfigurationBuilder();
+        //ConfigurationBuilder cb = new ConfigurationBuilder();
          
          
         //the following is set without accesstoken- desktop client
-        cb.setDebugEnabled(true)
-      .setOAuthConsumerKey("zgxDQpdlpONlRDZHaUVyzAKE0")
-      .setOAuthConsumerSecret("Vm4hoxq8D0DpU7ag540LCN36w8ZzmgmcKNpWjw1iJxVPb7UJog")
-      .setOAuthAccessToken(null)
-      .setOAuthAccessTokenSecret(null);
+        //cb.setDebugEnabled(true)
+    	//.setOAuthConsumerKey("zgxDQpdlpONlRDZHaUVyzAKE0")
+        //.setOAuthConsumerSecret("Vm4hoxq8D0DpU7ag540LCN36w8ZzmgmcKNpWjw1iJxVPb7UJog")
+        //.setOAuthAccessToken(null)
+        //.setOAuthAccessTokenSecret(null);
+      
         
         //ziurtatu acces token/secret behar den moduan dagoela
         //ziurtatu consumer key/secret behar den moduan daudela
@@ -44,29 +50,61 @@ public class Konexioa {
         
         //hemen zerbait dago: http://org/en/code-examples.html
         
-   
+        File file = new File("WinterIsComing.properties");
+        Properties prop = new Properties();
+        InputStream is = null;
+        OutputStream os = null;
         try {
-            TwitterFactory tf = new TwitterFactory(cb.build());
-            Twitter twitter = tf.getInstance();
+            if (file.exists()) {
+                is = new FileInputStream(file);
+                prop.load(is);
+                prop.setProperty("oauth.consumerKey", "zgxDQpdlpONlRDZHaUVyzAKE0");
+                prop.setProperty("oauth.consumerSecret", "Vm4hoxq8D0DpU7ag540LCN36w8ZzmgmcKNpWjw1iJxVPb7UJog");
+                System.out.println("ondo sortu deu");
+            }
+           if (args.length < 2) {
+                if (null == prop.getProperty("oauth.consumerKey")
+                        && null == prop.getProperty("oauth.consumerSecret")) {
+                    // consumer key/secret are not set in twitter4j.properties
+                    System.out.println(
+                            "Usage: java twitter4j.examples.oauth.GetAccessToken [consumer key] [consumer secret]");
+                    System.exit(-1);
+                }
+            } else {
+                
+                os = new FileOutputStream("WinterIsComing.properties");
+                prop.store(os, "WinterIsComing.properties");
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.exit(-1);
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException ignore) {
+                }
+            }
+            if (os != null) {
+                try {
+                    os.close();
+                } catch (IOException ignore) {
+                }
+            }
+        }
+        try {
+        	 Twitter twitter = new TwitterFactory().getInstance();
+             twitter.setOAuthAccessToken(null);
+             RequestToken requestToken = twitter.getOAuthRequestToken();
+             System.out.println("Got request token.");
+             System.out.println("Request token: " + requestToken.getToken());
+             System.out.println("Request token secret: " + requestToken.getTokenSecret());
+             AccessToken accessToken = null;
              
-            try {
  
-                // get request token.
-                // this will throw IllegalStateException if access token is already available
-                // this is oob, desktop client version
-                RequestToken requestToken = twitter.getOAuthRequestToken();
- 
-                System.out.println("Got request token.");
-                System.out.println("Request token: " + requestToken.getToken());
-                System.out.println("Request token secret: " + requestToken.getTokenSecret());
- 
-                System.out.println("|-----");
- 
-                AccessToken accessToken = null;
- 
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                  
-                while (null == accessToken) {
+               /* while (null == accessToken) {
                     System.out.println("Open the following URL and grant access to your account:");
                     System.out.println(requestToken.getAuthorizationURL());
                     try {
@@ -127,5 +165,65 @@ public class Konexioa {
             System.out.println("Failed to read the system input.");
             System.exit(-1);
         }
-    }
+    }*/
+             while (null == accessToken) {
+                 System.out.println("Open the following URL and grant access to your account:");
+                 System.out.println(requestToken.getAuthorizationURL());
+                 try {
+                     Desktop.getDesktop().browse(new URI(requestToken.getAuthorizationURL()));
+                 } catch (UnsupportedOperationException ignore) {
+                 } catch (IOException ignore) {
+                 } catch (URISyntaxException e) {
+                     throw new AssertionError(e);
+                 }
+                 System.out.print("Enter the PIN(if available) and hit enter after you granted access.[PIN]:");
+                 String pin = br.readLine();
+                 
+                 try {
+                     if (pin.length() > 0) {
+                         accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+                     } else {
+                         accessToken = twitter.getOAuthAccessToken(requestToken);
+                     }
+                 } catch (TwitterException te) {
+                     if (401 == te.getStatusCode()) {
+                         System.out.println("Unable to get the access token.");
+                     } else {
+                         te.printStackTrace();
+                     }
+                 }
+             }
+             System.out.println("Got access token.");
+             System.out.println("Access token: " + accessToken.getToken());
+             System.out.println("Access token secret: " + accessToken.getTokenSecret());
+             //hemetik segidu
+             try {
+                 prop.setProperty("oauth.accessToken", accessToken.getToken());
+                 prop.setProperty("oauth.accessTokenSecret", accessToken.getTokenSecret());
+                 os = new FileOutputStream(file);
+                 prop.store(os, "twitter4j.properties");
+                 os.close();
+             } catch (IOException ioe) {
+                 ioe.printStackTrace();
+                 System.exit(-1);
+             } finally {
+                 if (os != null) {
+                     try {
+                         os.close();
+                     } catch (IOException ignore) {
+                     }
+                 }
+             }
+             System.out.println("Successfully stored access token to " + file.getAbsolutePath() + ".");
+             System.exit(0);
+         } catch (TwitterException te) {
+             te.printStackTrace();
+             System.out.println("Failed to get accessToken: " + te.getMessage());
+             System.exit(-1);
+         } catch (IOException ioe) {
+             ioe.printStackTrace();
+             System.out.println("Failed to read the system input.");
+             System.exit(-1);
+         }
+     }
 }
