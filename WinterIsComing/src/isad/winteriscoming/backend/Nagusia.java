24036e1f8f8bc1ccbebde429828eed255e0f7074
Sergio <porobu@gmail.com>
2015-10-16 19:18:11 +0200

    ya tenemos una ventana to guapa

--- WinterIsComing/src/isad/winteriscoming/backend/Nagusia.java
+++ WinterIsComing/src/isad/winteriscoming/backend/Nagusia.java
@@ -3,7 +3,10 @@ package isad.winteriscoming.backend;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import isad.winteriscoming.frontend.WinterTwitter;
+
 public class Nagusia {
+	public static float BERTSIOA = 0.2F;
 
 	public static void main(String[] args) {
 		try {
@@ -11,6 +14,7 @@ public class Nagusia {
 		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
 				| UnsupportedLookAndFeelException e) {
 		}
+		new WinterTwitter();
 		DBKS.getDBKS().konektatu(DBKS.getDBKS().getPath());
 		Konexioa.getKonexioa().logeatu();
 	}
