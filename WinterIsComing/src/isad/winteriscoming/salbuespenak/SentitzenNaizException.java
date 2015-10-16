d5b41843d38c5aaabce0a21cf4238c67dccdb50b
Sergio <porobu@gmail.com>
2015-10-16 12:00:38 +0200

    datu basearen konexioa

--- WinterIsComing/src/isad/winteriscoming/salbuespenak/SentitzenNaizException.java
+++ WinterIsComing/src/isad/winteriscoming/salbuespenak/SentitzenNaizException.java
@@ -14,8 +14,7 @@ public class SentitzenNaizException extends RuntimeException {
 	public SentitzenNaizException(String gureMezua) {
 
 		try {
-			PrintWriter gurePW = new PrintWriter(new File(
-					System.getProperty("user.home") + "/SentitzenNaiz.txt"));
+			PrintWriter gurePW = new PrintWriter(new File(System.getProperty("user.home") + "/SentitzenNaiz.txt"));
 			gurePW.println("SENTITZEN NAIZ BAINA ERROREA!!!!!");
 			gurePW.println(gureMezua);
 			gurePW.println("Stack Trace:");
@@ -24,12 +23,9 @@ public class SentitzenNaizException extends RuntimeException {
 			gurePW.close();
 		} catch (FileNotFoundException sentitzenNaiz) {
 		}
-		JOptionPane.showMessageDialog(
-				null,
-				gureMezua + "\n" + "Stack Trace Laburra:\n"
-						+ ExceptionUtils.getRootCauseStackTrace(this)[1]
-						+ "\nStack trace " + System.getProperty("user.home")
-						+ "/SentitzenNaiz.txt" + " gorde da.",
+		JOptionPane.showMessageDialog(null,
+				gureMezua + "\n" + "Stack Trace Laburra:\n" + ExceptionUtils.getRootCauseStackTrace(this)[1]
+						+ "\nStack trace " + System.getProperty("user.home") + "/SentitzenNaiz.txt" + " gorde da.",
 				"SENTITZEN NAIZ BAINA ERROREA!!!!!", JOptionPane.ERROR_MESSAGE);
 	}
 }
