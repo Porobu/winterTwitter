0276f274ce91c1e3104f8d8f9526b38c92767a35
Sergio <porobu@gmail.com>
2015-10-28 10:17:21 +0100

    funciona a ratos

--- WinterIsComing/src/isad/winteriscoming/backend/DBKS.java
+++ WinterIsComing/src/isad/winteriscoming/backend/DBKS.java
@@ -1,10 +1,23 @@
 package isad.winteriscoming.backend;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
@@ -86,21 +99,80 @@ public final class DBKS {
 			System.exit(1);
 		fitxategia.delete();
 		try {
-			this.konexioa = DriverManager
-					.getConnection(UcanaccessDriver.URL_PREFIX + path + ";newdatabaseversion=V2010");
-		} catch (SQLException e) {
+			//String s = ExportResource("/isad/winteriscoming/datubasea.accdb", path);
+			//System.out.println(s);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Statement st;
+		  URI uri;
 		try {
-			st = this.konexioa.createStatement();
-			// TODO Aginduak jarri
-			st.execute(
-					"CREATE TABLE example1 (id  COUNTER PRIMARY KEY,descr text(400), number numeric(12,3), date0 datetime)");
-			st.close();
-		} catch (SQLException e) {
+			uri = DBKS.class.getResource("/isad/winteriscoming").toURI();
+			 Path myPath;
+			    if (uri.getScheme().equals("jar")) {
+			        FileSystem fileSystem = FileSystems.newFileSystem(uri, Collections.emptyMap());
+			        myPath = fileSystem.getPath("/isad/winteriscoming");
+			    } else {
+			        myPath = Paths.get(uri);
+			    }
+			    Stream<Path> walk = Files.walk(myPath, 1);
+			    for (Iterator<Path> it = walk.iterator(); it.hasNext();){
+			       JOptionPane.showMessageDialog(null, it.next());
+			    }
+			    walk.close();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		   
+	}
+
+	/**
+	 * Export a resource embedded into a Jar file to the local file path.
+	 *
+	 * @param resourceName
+	 *            ie.: "/SmartLibrary.dll"
+	 * @return The path to the exported resource
+	 * @throws Exception
+	 */
+	static public String ExportResource(String resourceName, String path) throws Exception {
+		InputStream stream = null;
+		OutputStream resStreamOut = null;
+		String jarFolder;
+		try {
+			stream = DBKS.class.getResourceAsStream(resourceName);// note that
+																	// each / is
+																	// a
+																	// directory
+																	// down in
+																	// the "jar
+																	// tree"
+																	// been the
+																	// jar the
+																	// root of
+																	// the tree
+			if (stream == null) {
+				throw new SentitzenNaizException("Cannot get resource \"" + resourceName + "\" from Jar file.");
+			}
+
+			int readBytes;
+			byte[] buffer = new byte[4096];
+			jarFolder = new File(DBKS.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())
+					.getParentFile().getPath().replace('\\', '/');
+			resStreamOut = new FileOutputStream(path);
+			while ((readBytes = stream.read(buffer)) > 0) {
+				resStreamOut.write(buffer, 0, readBytes);
+			}
+		} catch (Exception ex) {
+			throw new SentitzenNaizException("Ezin da fitxategia kopiatu");
+		} finally {
+			stream.close();
+			resStreamOut.close();
+		}
+
+		return jarFolder + resourceName;
 	}
 
 	public void konexioaItxi() {
