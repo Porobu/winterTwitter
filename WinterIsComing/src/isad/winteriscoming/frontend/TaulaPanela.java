1071a1ed13269d247017f430ee5380c14332fbb0
Sergio <porobu@gmail.com>
2015-12-04 12:36:10 +0100

    taulak++

--- WinterIsComing/src/isad/winteriscoming/frontend/TaulaPanela.java
+++ WinterIsComing/src/isad/winteriscoming/frontend/TaulaPanela.java
@@ -1,9 +1,21 @@
 package isad.winteriscoming.frontend;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class TaulaPanela extends JPanel {
 
 	private static final long serialVersionUID = 2602675352838564349L;
 
+	public TaulaPanela(String mota) {
+		this.setLayout(new BorderLayout(0, 1));
+		this.setOpaque(true);
+		TaulaModeloa gureModeloa = new TaulaModeloa(mota);
+		JTable gureTaula = new JTable(gureModeloa);
+		JScrollPane gureJScrollPane = new JScrollPane(gureTaula);
+		this.add(gureJScrollPane);
+	}
 }
