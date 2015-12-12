9d5c7a65f809fab4de4768622464577a573b515d
Sergio <porobu@gmail.com>
2015-12-12 16:10:51 +0100

    cambiar licencia

--- WinterIsComing/src/isad/winteriscoming/backend/Nagusia.java
+++ WinterIsComing/src/isad/winteriscoming/backend/Nagusia.java
@@ -1,7 +1,7 @@
 package isad.winteriscoming.backend;
 
 //    WinterTwitter Lizentzia
-//    Copyright (C) 2015  Sergio Pascual, Sergio Santana, Unai Arrizabalaga
+//    Copyright (C) 2015  Sergio Pascual, Sergio Santana, Unai Arrizabalaga, Jonathan Nieto
 //
 //    This program is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
